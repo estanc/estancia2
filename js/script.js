da7f5d9856dd9183e5d73bd3743240b806f95bed
estanc <013011809@upq.edu.mx>
2017-05-08 20:26:34 -0500

    index effects

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+	var n = 0;
+var l = document.getElementById("number");
+window.setInterval(function(){
+  l.innerHTML = n;
+  n++;
+  if (n==2000) {
+    l.innerHTML=20000;
+
+      }
+      break;
+}
+
+
+
+,0);
+});
