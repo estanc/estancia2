4b886a2e7de30f1d6007787c66179e9221b1658e
estanc <013011809@upq.edu.mx>
2017-05-21 14:51:24 -0500

    efectos en presencia nacional

--- js/script.js
+++ js/script.js
@@ -1,18 +1,6 @@
-$(document).ready(function() {
-
-	var n = 0;
-var l = document.getElementById("number");
-window.setInterval(function(){
-  l.innerHTML = n;
-  n++;
-  if (n==2000) {
-    l.innerHTML=20000;
-
-      }
-      break;
-}
-
-
-
-,0);
+$$(document).ready(function(){
+	$('section.PreNa .anima1').waypoint(function(){
+		$('section.PreNa .anima1').addClass('animated fadeIn');
+	});
+	
 });
